c90d8d7dead97883145a13a246dd1f56513a2868
espinozakaterin <katherineaguilar575@gmail.com>
2024-07-31 15:45:05 -0600

    grupo8

---
+++ variables.js
@@ -0,0 +1,90 @@
+
+const handlerBarsContext = {
+    "/index.html": {
+        "gallery": [
+            {
+                imgUrl: "https://images.unsplash.com/photo-1583934936900-1e678f8288a1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b2JyYXMlMjBkZSUyMGFydGV8ZW58MHwwfDB8fHww",
+                name: "Europa",
+               
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1581343109298-4a103c86d940?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8b2JyYXMlMjBkZSUyMGFydGV8ZW58MHwwfDB8fHww",
+                name: "libreria"
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1580197139365-9713573df381?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b2JyYXMlMjBkZSUyMGFydGV8ZW58MHwwfDB8fHww",
+                name: "playa",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1580136579295-b22833edc5e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8b2JyYXMlMjBkZSUyMGFydGV8ZW58MHwwfDB8fHww",
+                name: "Gemelas",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1579009721337-cec3c69778e4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG9icmFzJTIwZGUlMjBhcnRlfGVufDB8MHwwfHx8MA%3D%3D",
+                name: "Desenbarco",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1626438986057-4622590843a3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG9icmFzJTIwZGUlMjBhcnRlfGVufDB8MHwwfHx8MA%3D%3D",
+                name: "Luz",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1588260693037-45d3add2388d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fG9icmFzJTIwZGUlMjBhcnRlfGVufDB8MHwwfHx8MA%3D%3D",
+                name: "Naturaleza",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1581342996328-946ad4aa70af?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fG9icmFzJTIwZGUlMjBhcnRlfGVufDB8MHwwfHx8MA%3D%3D",
+                name: "Beisball",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1618336753974-aae8e04506aa?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YW5pbWV8ZW58MHwwfDB8fHww",
+                name: "Bebe Yoda (grogu)",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1580477667995-2b94f01c9516?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGFuaW1lfGVufDB8MHwwfHx8MA%3D%3D",
+                name: "Woody",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1660304755869-325c2ff6f02d?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fG1lc3NpfGVufDB8MHwwfHx8MA%3D%3D",
+                name: "Messi",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1561172317-5427a31ef91e?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG5hcnV0b3xlbnwwfDB8MHx8fDA%3D",
+                name: "Ciudad",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1598153346810-860daa814c4b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFycnklMjBwb3R0ZXJ8ZW58MHwwfDB8fHww",
+                name: "Hogwarts",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1476370648495-3533f64427a2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZGVhZCUyMG5vdGV8ZW58MHwwfDB8fHww",
+                name: "Calavera",
+                
+            },
+            {
+                imgUrl: "https://images.unsplash.com/photo-1578321270951-88bd84d09a64?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fG9icmFzJTIwZGUlMjBhcnRlfGVufDB8MHwwfHx8MA%3D%3D",
+                name: "Reunion",
+                
+            }
+        ]
+        
+        
+    },
+}
+
+const pageContext = (page)=>{
+    const context= {...handlerBarsContext[page]}
+    return context;
+}
+export default pageContext;
